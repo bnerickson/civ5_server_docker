db28a7fbc2a83194eba4d0da052e8f4bcdf8b659
Ben Erickson <bnerickson87@gmail.com>
2025-08-26 03:15:28 +0000

    Adding exit statements to scheduled_turn_notifications.bash

--- server/scheduled_turn_notifications.bash
+++ server/scheduled_turn_notifications.bash
@@ -2,8 +2,7 @@
 
 echo "Verifying TurnStatus-1.db file is non-empty"
 if [ ! -s "${CIV_DATA_ROOT}/ModUserData/TurnStatus-1.db" ]; then
-    sleep 3600
-    continue
+    exit 0
 fi
 
 PLAYER_STR=$(sqlite3 "${CIV_DATA_ROOT}/ModUserData/TurnStatus-1.db" "SELECT Value FROM SimpleValues WHERE Name = 'PlayersWhoNeedToTakeTheirTurn';")
@@ -13,7 +12,7 @@ fi
 
 TURN_NUM=$(sqlite3 "${CIV_DATA_ROOT}/ModUserData/TurnStatus-1.db" "SELECT Value FROM SimpleValues WHERE Name = 'TurnNum';")
 if [ ${?} -eq 0 ]; then
-    break
+    exit 0
 fi
 
 echo "Turn #: ${TURN_NUM}"
